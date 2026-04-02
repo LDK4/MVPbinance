222bbe9f470627b106fba1fc65f660f4e3832eb5
LDK4 <ldghoststudios@gmail.com>
2026-04-02 02:11:51 -0300

    Add files via upload
    slk

--- sw.js
+++ sw.js
@@ -1,4 +1,25 @@
-self.addEventListener('fetch', function(event) {
-  // Isso permite que o app funcione online, 
-  // no futuro você pode configurar para funcionar offline.
+const CACHE_NAME = 'mvp-balance-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icone-192.png'
+];
+
+// Instala o Service Worker e salva os arquivos no cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Intercepta as chamadas e serve o conteúdo do cache (se não tiver internet)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
