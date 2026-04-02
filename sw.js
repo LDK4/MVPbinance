a44d42c3f62604791b9e8ca45f61ddfeb8bc78f8
LDK4 <ldghoststudios@gmail.com>
2026-04-02 09:09:04 -0300

    Add files via upload
    21

--- sw.js
+++ sw.js
@@ -3,23 +3,35 @@ const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  './icone-192.png'
+  './icone-192.png',
+  './icone-512.png'
 ];
 
-// Instala o Service Worker e salva os arquivos no cache
-self.addEventListener('install', (event) => {
-  event.waitUntil(
+// Instalação
+self.addEventListener('install', (e) => {
+  e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
     })
   );
 });
 
-// Intercepta as chamadas e serve o conteúdo do cache (se não tiver internet)
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+// Ativação (Limpa outras versões como v2 ou v3)
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Estratégia de busca
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => {
+      return caches.match(e.request);
     })
   );
 });
